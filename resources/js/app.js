4bcddd47d58c6c6dd3068af6b69bf1f52ee061e8
Jing Zhang <jinginnz0122@gmail.com>
2019-11-12 17:51:18 +1300

    add base cms

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,34 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
-
-//import and use vuetify
-import Vuetify from 'vuetify'
-Vue.use(Vuetify);
  
 //import CSS of Vuetify
 import 'vuetify/dist/vuetify.min.css';
-import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css';
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import AdminApp from './admin/app.vue';
+import Vuetify from 'vuetify';
+import VueRouter from 'vue-router';
+import { adminRoutes } from './admin/routes/adminRoute.js';
+import { adminStore } from './admin/store/adminStore.js';
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const path = window.location.pathname;
+if (path.includes("admin")) {
 
-Vue.component('Sidebar', require('./components/Sidebar.vue').default);
+    Vue.use(Vuetify)
+    Vue.use(VueRouter)
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+    const adminRouter = new VueRouter({
+        mode: 'history',
+        base: '/admin',
+        routes: adminRoutes,
+    });
 
-const app = new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-});
+    const app = new Vue({
+        el: '#app',
+        vuetify: new Vuetify(),
+        render: h => h(AdminApp),
+        router: adminRouter,
+    });
+
+}
